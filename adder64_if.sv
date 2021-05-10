c7dd781827ad5e03c76e0af62e39d6725678b3dd
Vasanth Singh <vasanthsingh98@gmail.com>
2021-05-10 17:35:32 +0000

    Reorganized the modules into separate files

---
+++ adder64_if.sv
@@ -0,0 +1,11 @@
+// Interface for 64-bit adder
+    interface adder64_if;
+        logic [63:0] op1, op2;
+        logic [63:0] result;
+        logic carry_out, carry_in;
+
+        modport inst (
+            input op1, op2, carry_in,
+            output result, carry_out
+        );
+    endinterface
