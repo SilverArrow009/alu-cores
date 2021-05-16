fd9409d3dd0e4362cb46f958169fddf475f4425e
Vasanth Singh <vasanthsingh98@gmail.com>
2021-05-16 11:25:29 +0000

    Changed the formatting to comply with ifc2mod.tcl supported formatting

--- adder64_if.sv
+++ adder64_if.sv
@@ -1,8 +1,10 @@
 // Interface for 64-bit adder
     interface adder64_if;
-        logic [63:0] op1, op2;
+        logic [63:0] op1;
+        logic [63:0] op2;
         logic [63:0] result;
-        logic carry_out, carry_in;
+        logic carry_out;
+        logic carry_in;
 
         modport inst (
             input op1, op2, carry_in,
