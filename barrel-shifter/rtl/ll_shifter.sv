e31ff4c53448fd819c6094aa18993ee595df2234
Vasanth Singh <vasanthsingh98@gmail.com>
2021-05-23 12:41:43 +0000

    Added shifter interface and modified the signals accordingly

--- barrel-shifter/rtl/ll_shifter.sv
+++ barrel-shifter/rtl/ll_shifter.sv
@@ -1,7 +1,5 @@
 module ll_shifter (
-    input logic [63:0] in,    
-    input logic [5:0] shift_amount,
-    output logic [63:0] result
+    shifter_if.inst shift_if
 );
     stage_if if0, if1, if2, if3, if4, if5;
     stage_ll #(.SHAMT(1)) stage0 (if0.inst);
@@ -14,21 +12,21 @@ module ll_shifter (
     // Connect the interfaces
     always_comb begin : connection
         // Cascade the stages
-        if0.in = in;
+        if0.in = shift_if.in;
         if1.in = if0.out;
         if2.in = if1.out;
         if3.in = if2.out;
         if4.in = if3.out;
         if5.in = if4.out;
-        result = if5.out;
+        shift_if.result = if5.out;
         
         // Assign the control signals
-        if0.sig = shift_amount[0];
-        if1.sig = shift_amount[1];
-        if2.sig = shift_amount[2];
-        if3.sig = shift_amount[3];
-        if4.sig = shift_amount[4];
-        if5.sig = shift_amount[5];
+        if0.sig = shift_if.shift_amount[0];
+        if1.sig = shift_if.shift_amount[1];
+        if2.sig = shift_if.shift_amount[2];
+        if3.sig = shift_if.shift_amount[3];
+        if4.sig = shift_if.shift_amount[4];
+        if5.sig = shift_if.shift_amount[5];
 
     end
-endmodule
+endmodule : ll_shifter
