8b104f67745a76d381f648843aeaf7649e8cdf7d
Vasanth Singh <vasanthsingh98@gmail.com>
2021-05-24 14:40:40 +0000

    Added support for bit rotations and taking manual inputs

--- barrel-shifter/tb/barrel_shifter64_tb.cpp
+++ barrel-shifter/tb/barrel_shifter64_tb.cpp
@@ -3,9 +3,20 @@
 #include <iostream>
 #include <bits/stdc++.h>
 
-#define NUM_TEST_CASES 50
+#define NUM_TEST_CASES 4
+#define TEST_MODE "MANUAL"
 
 using namespace std;
+
+long bit_rotate (long in, int shamt, bool dir) {
+    if(dir) {   // rotate right
+        long result = (in >> shamt) | (in << (64-shamt));
+        return result;
+    } else {
+        long result = (in << shamt) | (in >> (64-shamt));
+        return result;
+    }
+}
 
 int main (int argc, char** argv, char** env)
 {
@@ -18,16 +29,22 @@ int main (int argc, char** argv, char** env)
     for (int i = 0; i < NUM_TEST_CASES; i++)
     {
         // Choose random inputs
-        in = rand();
-        shamt = rand() % 64;
-        mode = rand() % 2;
+        if(TEST_MODE == "RANDOM") {
+            in = rand();
+            shamt = rand() % 64;
+            mode = rand() % 4;
+        } else {
+            in = 0x80000000;
+            shamt = 1;
+            mode = 5;
+        }
         // calculate result
         switch (mode)
         {
-            case 0: result = in << shamt; break;
-            case 1: result = in >> shamt; break;
-            // case 2: result = in <<< shamt; break;
-            // case 3: result = in <<< shamt; break;
+            case 1: result = in >> shamt; break;    // right shift
+            case 4: result = bit_rotate(in, shamt, false); break;   // left rotate
+            case 5: result = bit_rotate(in, shamt, true); break;    // right rotate
+            default: result = in << shamt; break; // left shift
         }
         // Assign the inputs
         dut->in = in;
@@ -36,11 +53,11 @@ int main (int argc, char** argv, char** env)
         // Evaluate the model
         dut->eval();
         // Report the results
-        printf("in = %ld, shift_amount = %ld, shift_type = %d\t", in, shamt, mode );
+        printf("in = %x, shift_amount = %ld, shift_type = %x\t", in, shamt, mode );
         if (result == dut->result) {
             cout << "OUTPUT MATCHES" << endl;
         } else {
-            printf("OUTPUT MISMATCHES. Expected result = %ld, actual result = %ld\n", result, dut->result );
+            printf("OUTPUT MISMATCHES. Expected result = %x, actual result = %x\n", result, dut->result );
         }
     }
     // Free up the allocated space
