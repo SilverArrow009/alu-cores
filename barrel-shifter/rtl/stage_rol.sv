ea6ef330e14ea8d33d255b49cab5d00f053d07d7
Vasanth Singh <vasanthsingh98@gmail.com>
2021-05-24 14:39:01 +0000

    Fixed the bug caused due to faulty assignments in always block

--- barrel-shifter/rtl/stage_rol.sv
+++ barrel-shifter/rtl/stage_rol.sv
@@ -3,8 +3,8 @@ module stage_rol #(parameter SHAMT = 1) (
 );
     always_comb begin
         if (if0.sig) begin : rotate_left
-                if0.out[63:SHAMT] = if0.in[63:SHAMT];
-                if0.out[SHAMT-1:0] = if0.in[SHAMT-1:0];
+                if0.out[63:SHAMT] = if0.in[63-SHAMT:0];
+                if0.out[SHAMT-1:0] = if0.in[63:63-SHAMT+1];
             end
         else
             if0.out = if0.in;
